0477b30c03a65ecbf9b4cc8545189c53111e9a65
Alisson <alissonhenrique98@gmail.com>
2020-03-11 20:37:23 -0300

    primeiro commit

---
+++ src/br/com/collection/Main.java
@@ -0,0 +1,17 @@
+package br.com.collection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		
+		
+	}
+
+}
