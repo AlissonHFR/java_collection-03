15fdcf7ef94d1f3ff312443d633632fe64251a28
Alisson <alissonhenrique98@gmail.com>
2020-03-16 20:01:43 -0300

    primeiro construtor

--- src/br/com/collection/Cachorro.java
+++ src/br/com/collection/Cachorro.java
@@ -2,6 +2,12 @@ package br.com.collection;
 
 public class Cachorro extends Animal{
 
-	
+	public Cachorro(String nome) {
+		
+	}
+	@Override
+	public void som() {
+		System.out.println("Eu gosto de latir");
+	}
 
 }
